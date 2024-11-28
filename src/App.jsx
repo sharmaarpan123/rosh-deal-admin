46ae0315dc5297551257c232413dd7364d8abbc8
sharmaarpan143 <arpan.sharma@suffescom.com>
2024-11-28 23:55:13 +0530

    clean up

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,48 @@
-import "./App.css";
-import { Routes, Route, Navigate } from "react-router-dom";
-import { privateRoutes, routes } from "./pages/index";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./Assets/css/style.css";
+import { useSelector } from "react-redux";
+import { Navigate, Route, Routes } from "react-router-dom";
+import "./App.css";
 import "./Assets/css/responsive.css";
+import "./Assets/css/style.css";
 import AuthLayout from "./layout/Auth/authLayout";
 import MainLayout from "./layout/MainLayout/MainLayout";
-import { useSelector } from "react-redux";
-
+import { privateRoutes, routes } from "./pages/index";
 
 function App() {
   const isAuthenticated = useSelector((s) => s.login.token);
 
   return (
-    <Routes>
-      {isAuthenticated ? (
-        <Route element={<MainLayout />}>
-          <Route path="*" element={<Navigate replace to="/dashboard" />} />
-          {privateRoutes.map((data, index) => (
-            <Route
-              onUpdate={() => window.scrollTo(0, 0)}
-              exact={true}
-              path={data.path}
-              element={data.component}
-              key={index}
-            />
-          ))}
-        </Route>  
-      ) : (
-        <Route element={<AuthLayout />}>
-          <Route path="*" element={<Navigate to="/login" />} />
-          {routes.map((data, index) => (
-            <Route
-              onUpdate={() => window.scrollTo(0, 0)}
-              exact={true}
-              path={data.path}
-              element={data.component}
-              key={index}
-            />
-          ))}
-        </Route>
-      )}
-    </Routes>
+    <>
+      <Routes>
+        {isAuthenticated ? (
+          <Route element={<MainLayout />}>
+            <Route path="*" element={<Navigate replace to="/dashboard" />} />
+            {privateRoutes.map((data, index) => (
+              <Route
+                onUpdate={() => window.scrollTo(0, 0)}
+                exact={true}
+                path={data.path}
+                element={data.component}
+                key={index}
+              />
+            ))}
+          </Route>
+        ) : (
+          <Route element={<AuthLayout />}>
+            <Route path="*" element={<Navigate to="/login" />} />
+            {routes.map((data, index) => (
+              <Route
+                onUpdate={() => window.scrollTo(0, 0)}
+                exact={true}
+                path={data.path}
+                element={data.component}
+                key={index}
+              />
+            ))}
+          </Route>
+        )}
+      </Routes>
+    </>
   );
 }
 
